332095c0c2bcdbc9ccd7748ada9c32607605da0e
gaodashen0928 <982554623@qq.com>
2023-12-02 21:57:12 +0800

    分支测试

---
+++ dome1/src/main/java/MainTest1.java
@@ -0,0 +1,8 @@
+public class MainTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello Word");
+        System.out.println("test change");
+        System.out.println(" git checkout ");
+    }
+}
